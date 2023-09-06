703dabab3c3004bd2dbe72f7ca11d68d2099ba33
Kinh Nguyen <nguyenkinh@ytecongcong.com>
2023-09-06 18:17:11 +0200

    move to header

--- inst/include/ordinal.hpp
+++ inst/include/ordinal.hpp
@@ -1,3 +1,44 @@
+// Copyright (c) 2023 Kinh Nguyen
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+
+/**
+ * Fill covariance matrix with a vector of correlation 
+*/
+template <class Type>
+matrix<Type> fill_covariance(matrix<Type> M, vector<Type> v) {
+  int n_col = M.cols(), count = 0; // TODO check square, check v's len
+  for (int r = 0; r < n_col - 1; ++r)
+    for (int c = 0; c < n_col; ++c) {
+      M(r,c) = v[count];
+      M(c,r) = v[count];
+      ++count;
+    }
+  return M;
+}
+/**
+ * Cummulative link for ordinal
+ * 
+ * Only logit and probit for now, to do multivariate model
+ * 
+*/
 template <class Type>
 Type CDF_polr(Type x, int link = 1) {
   // link 1 logit 2 probit
@@ -55,7 +96,9 @@ vector<Type> make_cutpoints(vector<Type> probabilities, int link = 1) {
   return cutpoints;
 }
 
-// transform unbounded to a simplex 
+/** 
+ * transform unbounded to a simplex for paraneter in ordinal model
+*/
 template<class Type>
 vector<Type> simplex_transform(vector<Type> y) {
   int K = y.size() + 1;
@@ -68,4 +111,24 @@ vector<Type> simplex_transform(vector<Type> y) {
     x[k] = (1 - x.head(k).sum()) * z[k]; // head index is different
   x[K-1] = 1 - sum(x);
   return x;
+}
+
+/**
+ * Export Fortran function and register for TMB
+ * 
+ * See Kasper's email
+*/
+extern "C" {
+  double F77_NAME(mvbvn)(void *lower, void *upper, int *infin, void *correl);
+  const static R_CallMethodDef R_CallDef[] = {
+    TMB_CALLDEFS,
+    {"mvbvn", (DL_FUNC) &F77_NAME(mvbvn), 4},
+    {NULL, NULL, 0}
+  };
+  void R_init_ordinalTMB(DllInfo *dll) 
+  {
+    R_registerRoutines(dll, NULL, R_CallDef, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+    TMB_CCALLABLES("ordinalTMB");
+  }
 }
